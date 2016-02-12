c1a3200816ac8f0d7108d282dac83eed3510d171
Chris Slamar <chris@slamar.com>
2016-02-12 13:33:08 -0600

    added more filtering and info recv

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,7 @@
 var encounterApp = angular.module('encounterApp', []);
 
 encounterApp.controller('EncounterCtrl', function ($scope, $http) {
+  var name_array = [];
   $http.get('http://e.slamar.com/api/v1/monsters/').success(function(data) {
     $scope.monsters = data.monsters;
     // console.log(data);
@@ -8,6 +9,27 @@ encounterApp.controller('EncounterCtrl', function ($scope, $http) {
 
   $http.get('data/names_set.json').success(function(data){
     $scope.random_names = data.names_set;
-    // console.log(data.names_set);
+    name_array = data.names_set;
+
+    var seed = Math.round((Math.random() * 1000) % data.names_set.length);
+
+    console.log(data.names_set[seed]);
+
+    $scope.random_name = data.names_set[seed];
   });
+
+  $scope.addName = function(){
+    var seed = Math.round((Math.random() * 1000) % name_array.length);
+    $scope.some_name = name_array[seed];
+  };
+
+  $scope.getMonsterInfo = function(id) {
+    console.log('Getting monster info...');
+    // ('http://e.slamar.com/api/v1/monsters/' + id +'/')
+    $http.get('http://e.slamar.com/api/v1/monsters/' + id +'/').success(function(data){
+      console.log(data);
+      $scope.selectedMonster = data;
+    });
+  }
+
 });
