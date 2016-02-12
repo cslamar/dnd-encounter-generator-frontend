72e501afccfeb8d6a68067e1030d7122061ee80e
Chris Slamar <chris@slamar.com>
2016-02-12 12:46:37 -0600

    added random names

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,11 @@ var encounterApp = angular.module('encounterApp', []);
 encounterApp.controller('EncounterCtrl', function ($scope, $http) {
   $http.get('http://e.slamar.com/api/v1/monsters/').success(function(data) {
     $scope.monsters = data.monsters;
-    console.log(data);
+    // console.log(data);
   });
 
+  $http.get('data/names_set.json').success(function(data){
+    $scope.random_names = data.names_set;
+    // console.log(data.names_set);
+  });
 });
